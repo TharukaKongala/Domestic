bab1eb91787b610b4931b778c57f1c0fcb9707c5
ACER <kongalatharuka@gmail.com>
2022-12-29 14:58:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/domestic/ActivityBill.java
@@ -0,0 +1,14 @@
+package com.example.domestic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityBill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+    }
+}
---
+++ app/src/main/java/com/example/domestic/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.domestic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etSelectDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etSelectDate = findViewById(R.id.etSelectDate);
+
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        etSelectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        month = month+1;
+                        String date = dayOfMonth+"/"+month+"/"+year;
+                        etSelectDate.setText(date);
+
+                    }
+                },year, month,day);
+                dialog.show();
+
+            }
+        });
+
+    }
+}
